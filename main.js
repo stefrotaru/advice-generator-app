4fd5493b73fb015f7a7bad19774719761e57444c
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-11-21 15:08:42 +0200

    Got rid of the hard coded data, added logic for populating the advice id and text on page load

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const rndmBtn = document.querySelector('.advice-container__button');
 const adviceIdHolder = document.getElementById('advice-no');
 const adviceTextHolder = document.getElementById('advice-text');
 
-
-rndmBtn.addEventListener('click', async () => {
-        const response = await fetch('https://api.adviceslip.com/advice');
+const getRandomAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
         const adviceJson = await response.json();
 
         adviceIdHolder.textContent = adviceJson.slip.id;
         adviceTextHolder.textContent = adviceJson.slip.advice;
-    })
+}
 
+// Populate targeted fields with the response from the API call on page load
+window.onload = getRandomAdvice; 
+
+// Populate targeted fields with the response from the API call on button click
+rndmBtn.addEventListener('click', getRandomAdvice);
 
